421670becdd40271e09990e7bfb25774f74e3384
Brenno Givigier <35437262+brennogf@users.noreply.github.com>
2020-08-14 12:38:15 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+require('dotenv').config({  
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+})
 
 const api = axios.create({ baseURL: process.env.REACT_APP_URL });
 
